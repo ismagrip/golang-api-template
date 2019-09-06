ede18b85346fcc2e1788b41d7a6c6f975e091ae5
Chris Magripis <cmagripis@gmail.com>
2019-09-06 11:04:52 +0300

    version 1.0

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,15 @@
 import subprocess
 import os
 Project_path = os.path.realpath(os.path.curdir)
+project_name='{{cookiecutter.project_name}}'
+git_host = '{{cookiecutter.git_hoster}}'
+owner = '{{cookiecutter.owner}}'
+git_name = '{{cookiecutter.git_name}}'
 subprocess.call(["git","init"])
 subprocess.call(["git","add","*"])
 subprocess.call(["git","commit", "-m","Initial commit"])
-# subprocess.call(["git","remote","add","origin", "https://"+Project_path])
-# subprocess.call(["git","push","-u","origin","master"])
-# subprocess.call(["go","mod","init"])
+subprocess.call(["git","config","user.name",git_name])
+subprocess.call(["git","remote","add","origin", "https://"+git_host+"/"+owner+"/"+project_name+".git"])
+subprocess.call(["git","push","-u","origin","master"])
+subprocess.call(["go","mod","init"])
+subprocess.call(["go","get"])
